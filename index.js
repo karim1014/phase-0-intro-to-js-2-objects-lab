a73b6a7fbc22df4061115dc26b3ba31783f6d455
karim rezk <bigkoko882@gmail.com>
2023-03-11 17:51:38 -0500

    complete objects lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+// initialize employee object
+const employee = {
+    name: "John",
+    streetAddress: "123 Main St"
+  };
+  
+  // function to update employee object non-destructively
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+  // function to update employee object destructively
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // function to delete key-value pair from employee object non-destructively
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // function to delete key-value pair from employee object destructively
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
